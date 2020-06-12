78e4ddc74c7837425721f3d30cf0cc300a1a8073
Faisal Hatila <hatilas1994@gmail.com>
2020-06-12 23:51:32 +0500

    cover component emported and exported in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,10 @@ import _Header from "./header";
 import _Footer from "./footer";
 import _Layout from "./layout";
 import _Navbar from "./navbar";
+import _Cover from "./cover";
 
 export const Header = _Header;
 export const Footer = _Footer;
 export const Layout = _Layout;
 export const Navbar = _Navbar;
+export const Cover = _Cover;
