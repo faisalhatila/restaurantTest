8ca4b178f9d57600927fd7e1fc5667d5258736ff
Faisal Hatila <hatilas1994@gmail.com>
2020-06-12 23:49:36 +0500

    cover component created

---
+++ src/components/cover/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class Cover extends Component {
+  render() {
+    return (
+      <div style={{ backgroundImage: "url(assets/img/cover.jpeg)" }}></div>
+    );
+  }
+}
