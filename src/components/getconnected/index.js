5bfbeab4929325be66b8aef6d2bd0f27302c3f7f
Faisal Hatila <hatilas1994@gmail.com>
2020-06-13 21:10:54 +0500

    get connected component, hover animation done

--- src/components/getconnected/index.js
+++ src/components/getconnected/index.js
@@ -9,31 +9,31 @@ export default class GetConnected extends Component {
           <p>in any you want to</p>
         </div>
         <div className="getConnectedComponentRow2 row mt-3">
-          <div className="col-12 col-lg-4 col-md-4">
+          <div className="col-12 col-lg-4 col-md-4 getConnectedColmn">
             <div className="getConnectedRow2Col getConnectedRow2Col1">
               <i class="fas fa-car"></i>
             </div>
-            <h3 className="getConnectedContentHeading">Customers</h3>
+            <h3 className="getConnectedContentHeading mt-3">Customers</h3>
             <p className="getConnectedContentPara">
               It is a long established fact that a reader will be distracted by
               the readable content of a page
             </p>
           </div>
-          <div className="col-12 col-lg-4 col-md-4">
+          <div className="col-12 col-lg-4 col-md-4 getConnectedColmn">
             <div className="getConnectedRow2Col getConnectedRow2Col2">
               <i class="fas fa-car"></i>
             </div>
-            <h3 className="getConnectedContentHeading">Restaurants</h3>
+            <h3 className="getConnectedContentHeading mt-3">Restaurants</h3>
             <p className="getConnectedContentPara">
               It is a long established fact that a reader will be distracted by
               the readable content of a page
             </p>
           </div>
-          <div className="col-12 col-lg-4 col-md-4">
+          <div className="col-12 col-lg-4 col-md-4 getConnectedColmn">
             <div className="getConnectedRow2Col getConnectedRow2Col3">
               <i class="fas fa-car"></i>
             </div>
-            <h3 className="getConnectedContentHeading">Drivers</h3>
+            <h3 className="getConnectedContentHeading mt-3">Drivers</h3>
             <p className="getConnectedContentPara">
               It is a long established fact that a reader will be distracted by
               the readable content of a page
