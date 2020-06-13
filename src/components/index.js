011a793826420fa2817ac1457c77ae7f12694455
Faisal Hatila <hatilas1994@gmail.com>
2020-06-13 05:02:10 +0500

    get connected component, import and export

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,11 @@ import _Footer from "./footer";
 import _Layout from "./layout";
 import _Navbar from "./navbar";
 import _Cover from "./cover";
+import _GetConnected from "./getconnected";
 
 export const Header = _Header;
 export const Footer = _Footer;
 export const Layout = _Layout;
 export const Navbar = _Navbar;
 export const Cover = _Cover;
+export const GetConnected = _GetConnected;
